1edc3be23142172489b0f704fb532d99e2f75d23
RohanMishra-179 <kingbooker124@gmail.com>
2025-08-14 09:09:48 +0530

    learning gitlens

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -11,10 +11,12 @@ const Login = () => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
+  //git lens check 1
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setError('');
-
+    //git lens check 2
     try {
       const { data } = await api.post('http://localhost:5000/api/auth/login', form);
       console.log('Login response data:', data);
